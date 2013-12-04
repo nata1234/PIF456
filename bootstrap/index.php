84fd157a329a5d8e012dc16e795c82e75dab4c92
Miftahul Qiki Winata <conan_winata@yahoo.com>
2013-12-04 12:02:26 +0700

    modul 5

---
+++ bootstrap/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Bootstrap</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Bootstrap -->
+		<link href="dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="dist/css/bootstrap.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        <nav class="navbar navbar-default" role="navigation">
+
+		<div class="navbar navbar-inverse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="http://localhost/takul/bootstrap/index.php">Home....</a></li>
+				<li class="active"><a href="http://localhost/takul/bootstrap/login.php">Login</a></li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+		<script src="http://code.jquery.com/jquery.js"></script>
+		<script src="dist/js/bootstrap.min.js"></script>
+		<script src="dist/js/jquery.js"></script>
+		<script src="dist/js/bootstrap-dropdown.js"></script>
+		<script src="dist/js/bootstrap-collapse.js"></script>
+		<script src="dist/js/application.js"></script>
+		<script src="dist/js/bootstrap-affix.js"></script>
+		<script src="dist/js/bootstrap-alert.js"></script>
+		<script src="dist/js/bootstrap-button.js"></script>
+		<script src="dist/js/bootstrap-carousel.js"></script>
+		<script src="dist/js/bootstrap-modal.js"></script>
+		<script src="dist/js/bootstrap-popover.js"></script>
+		<script src="dist/js/bootstrap-scrollspy.js"></script>
+		<script src="dist/js/bootstrap-tab.js"></script>
+		<script src="dist/js/bootstrap-tooltip.js"></script>
+		<script src="dist/js/bootstrap-typeahead.js"></script
+		
+		<div id="data">
+			<table align="center" cellpadding="25">
+				<tr> <h1 align="center"> Tugas Bootstrap </h1> </tr>
+				<tr> <h1 align="center"> Miftahul Qiki Winata </h1> </tr>
+				<tr> <h1 align="center"> S1 PTI B </h1> </tr>
+				<tr> <h1 align="center"> 110533430520 </h1> </tr>
+			</table>
+		</div>
+	</body>
+</html>
